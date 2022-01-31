02d64021e94f2163e74b2d68a8016a9f914aec44
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-01-30 19:10:39 -0800

    Update room.h

--- room.h
+++ room.h
@@ -1,1 +1,25 @@
+#pragma once
+#include <vector>
+#include <map>
+#inlcude "item.h"
 
+using namespace std;
+
+class room{
+
+  public:
+    char* des;
+    char* label;
+    vector<item*> items;
+    map<char*, room*> neighbors;
+    room(char*,char*);
+    room* getNextRoom(char*);
+    room* getItem(char*);
+    void addItem(item*);
+    void printTheItems();
+    void printTheExits();
+    void addAround(char*, room*);
+    char* getDes;
+    char* getLabel;
+    void removeItem(char*);
+}
