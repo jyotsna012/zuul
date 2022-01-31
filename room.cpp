2bd3dfbe57401cf7033b126ef25c7dbb51f41042
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-01-30 19:29:29 -0800

    Update room.cpp

--- room.cpp
+++ room.cpp
@@ -4,5 +4,71 @@
 #inlcude <vector>
 #include "room.h"
 
-using
+using namespace std;
+
+room::room(char* nLabel, char* nDes){
+  des = new char[200]
+  label = new char[200]
+  strcpy(label, nLabel);
+  strcpy(des, nDes);
+}
+
+void room::printTheItems(){
+  cout << "here are the items in this room: " << endl;
+  for(vector<item*> :: interator it = items.begin(); it != items.end(); i++){
+    cout << (*it)->getLabel() << endl;
+  }
+}
+
+void room::printTheExits(){
+  map<char*,Room*>::iterator it;
+  for(it = neighbors.begin(); it != neighbors.end(); it++){
+    cout << it->first << endl;
+  }
+}
+
+room* room::getNextRoom(char* d){
+  map<char*,room*>::iterator it;
+  for(it = neighbors.begin(); it != neighbors.end(); it++){
+    if(strcmp((it->first),d)==0){
+      return it->second;
+    }
+  }
+  return NULL;
+}
+
+item* room::getItem(char* label){
+  for(vector<item*>::iterator it = items.begin(); it!=items.end(); it++){
+    char* itemlabel = (*it)->getLabel();
+    if(strcmp(itemlabel,label)==0){
+      return (*it);
+    }
+  }
+  return NULL;
+}
+
+void room::removeItem(char* label){
+  for(vector<item*>::iterator it = items.begin(); it!=items.end(); it++){
+    char* itemlabel = (*it)->getLabel();
+    if(strcmp(itemlabel,label)==0){
+      items.erase(it);
+      break;
+    }
+  }
+}
+
+void room::addItem(item* i){
+  items.push_back(i);
+}
+
+char* Room::getDes(){
+  return des;
+}
+
+char* Room::getLabel(){
+  return label;
+}
+void Room::addAround(char* d, room* n){
+  neighbors.insert({d,n});
+}
 
