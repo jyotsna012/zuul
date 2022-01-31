beb2355922f3381e6f7c10aa543a742cd3ee7f2f
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-01-30 19:59:45 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,87 +9,85 @@ using namespace std;
 
 int main(){
   
-  char item[200] = "White Board";
+  char item[200] = " Paper Towel ";
   char label[200] = "French Class";
-  char description[200] = "Bonjour! You are in the French Class... Yayyy!!";
-  room* frenchclass = new Room(label, description);
+  char des[200] = "Bonjour! You are in the French Class... Yayyy!!";
+  room* frenchclass = new room(label, des);
 
   strcpy(label,"bathroom");
-  strcpy(description ,"You are in the bathroom. Unlike the school one, this one smells tolerable. Is that lavender?");
-  Room* bathroom = new Room(label,description);
-  strcpy(item,"SOAP");
-  Item* soap = new Item(item);
-  bathroom->addItem(soap);
+  strcpy(des,"You are now in the cleanest bathroom");
+  room* bathroom = new room(label,des);
+  item* papertowel = new item(item);
+  bathroom->addItem(papertowel);
   
-  strcpy(label,"pool");
-  strcpy(description ,"You have entered the pool room. Woah the floor is slippery, don't fall in!");
-  Room* pool = new Room(label,description);
+  strcpy(label,"library");
+  strcpy(item, "book");
+  strcpy(des,"Shh... everyone is studying in thr library");
+  room* library = new room(label,des);
+  item* book = new item(item);
+  library->addItem(book);
   
-  strcpy(label,"hot tub");
-  strcpy(description ,"You are in the hot tub room. It's so hot in here, and the steam is so thick you can barely see!");
-  Room* hottub = new Room(label,description);
+  strcpy(label,"Bio Class");
+  strcpy(item, "microscope");
+  strcpy(des,"Ohh is that connective tissue I see under the microscope in the Bio Class");
+  room* bioclass = new room(label,des);
+  item* microscope = new item(item);
+  bioclass->addItem(microscope);
   
-  strcpy(label,"roller coaster");
-  strcpy(description ,"You are in the roller coaster room. Wow, could the rooms get any weirder? Wait is that kid falling?");
-  Room* rollercoaster = new Room(label,description);
+  strcpy(label,"Computer Class");
+  strcpy(item, "computer");
+  strcpy(des,"lets program zuul in the computer lab");
+  room* computerclass = new room(label,des);
+  item* computer = new item(item);
+  computerclass->addItem(computer);
   
-  strcpy(label,"animal farm");
-  strcpy(description ,"You are in the animal farm. Is it just me or is that bull coming towards you a little too fast...");
-  Room* animalfarm = new Room(label,description);
+  strcpy(label,"Drama Room");
+  strcpy(item, "costumes");
+  strcpy(des,"we are in the drama room");
+  room* dramaroom = new room(label,des);
+  item* costumes = new item(item);
+  dramaroom->addItem(costumes);
   
-  strcpy(label,"ball pit");
-  strcpy(description ,"You've fallen into the ball pit. Getting out will require immense effort. Who knew this would be so difficult?");
-  Room* ballpit = new Room(label,description);
-  strcpy(item,"BALL");
-  Item* ball = new Item(item);
-  ballpit->addItem(ball);
   
-  strcpy(label,"cafeteria");
-  strcpy(description ,"You are in the cafeteria. Remnants of food fights still stain the walls.");
-  Room* cafeteria = new Room(label,description);
+  strcpy(label,"Math Class");
+  strcpy(des,"You are in everyone's most favorite class...not...math ugh!");
+  room* mathclass = new room(label,des);
   
-  strcpy(label,"ice cream");
-  strcpy(description ,"You are in the ice cream shop. Ooh birthday cake, your favorite!");
-  Room* icecream = new Room(label,description);
+  strcpy(label,"Chem Class");
+  strcpy(des ,"Whoa, I see something smoking in the chem class - must be a cool reaction");
+  room* chemclass = new room(label,des);
   
-  strcpy(label,"indoor beach");
-  strcpy(description ,"You are in the indoor beach. In case you don't know what that is, it's like a beach, but indoors");
-  Room* indoorbeach = new Room(label,description);
-  strcpy(item,"SHOVEL");
-  Item* shovel = new Item(item);
-  indoorbeach->addItem(shovel);
+  strcpy(label,"Physics Class");
+  strcpy(des ,"We are in the Physics Class");
+  room* physicsclass = new room(label,des);
   
-  strcpy(label,"shaved ice");
-  strcpy(description ,"You are at the shaved ice shop! Who knew strawberry and root beer would go together so well!");
-  Room* shavedice = new Room(label,description);
+  strcpy(label,"Lit Class");
+  strcpy(des ,"We are in the Lit Class");
+  room* litclass = new room(label,des);
   
-  strcpy(label,"kitchen");
-  strcpy(description ,"You are in the kitchen. There's no way that lump of tasteless flour becomes bread.");
-  Room* kitchen = new Room(label,description);
-  strcpy(item,"KNIFE");
-  Item* knife = new Item(item);
-  kitchen->addItem(knife);
+  strcpy(label,"History Class");
+  strcpy(des ,"We are in the History Class");
+  room* historyclass = new room(label,des);
   
-  strcpy(label,"tree house");
-  strcpy(description ,"You are in the tree house! so. high. above. the. ground. Don't fall!");
-  Room* treehouse = new Room(label,description);
+  strcpy(label,"Japanese Class");
+  strcpy(des ,"We are in the Japanese Class");
+  room* japaneseclass = new room(label,des);
   
-  strcpy(label,"puppy room");
-  strcpy(description ,"You are in the puppy room. You want to leave, but how can anyone say no to those eyes?");
-  Room* puppyroom = new Room(label,description);
-  strcpy(item,"PUPPY");
-  Item* puppy = new Item(item);
-  puppyroom->addItem(puppy);
+  strcpy(label,"Spanish Class");
+  strcpy(des ,"We are in the Spanish Class");
+  room* spanishclass = new room(label,des);
   
-  strcpy(label,"trampoline");
-  strcpy(description ,"You are in the trampoline room. Wow it feels like you're on the moon! Watch out for the ceiling!");
-  Room* trampoline = new Room(label,description);
+  strcpy(label,"GloPo Class");
+  strcpy(des ,"We are in the GloPo Class");
+  room* glopoclass = new room(label,des);
   
-  strcpy(label,"theater");
-  strcpy(description ,"You are in the indoor theater. Your shoes are probably going to have popcorn all over them for the next month or so.");
-  Room* theater = new Room(label,description);
+  strcpy(label,"Cafe");
+  strcpy(des ,"We are in the Cafe");
+  room* cafe = new room(label,des);
   
-
+  strcpy(label,"Gym");
+  strcpy(des ,"We are in the Gym");
+  room* gym = new room(label,des);
   
   rooms.push_back(bathroom);
   rooms.push_back(mathclass);
@@ -101,7 +99,7 @@ int main(){
   rooms.push_back(frenchclass);
   rooms.push_back(japaneseclass);
   rooms.push_back(spanishclass);
-  rooms.push_back(globalpolitics);
+  rooms.push_back(glopoclass);
   rooms.push_back(computerclass);
   rooms.push_back(cafe);
   rooms.push_back(library);
