67fab17c7e72bf7f8d6ddf5a38af73b151ba0377
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-01-31 09:40:24 -0800

    Update room.h

--- room.h
+++ room.h
@@ -8,19 +8,29 @@ using namespace std;
 class room{
 
   public:
+    //description of room
     char* des;
+    //name of room
     char* label;
+    //items in room
     vector<item*> items;
     room(char*,char*);
+    //gets the next room based on direction
     room* getNextRoom(char*);
+    //gets the item
     item* getItem(char*);
+    //add item to room
     void addItem(item*);
+    //print the items
     void printTheItems();
+    //print the exits in the room
     void printTheExits();
     void addAround(char*,room*);
     char* getDes();
     char* getLabel();
     void removeItem(char*);
+    //map of neighbors
     map<char*,room*> neighbors;
 
 };
+
